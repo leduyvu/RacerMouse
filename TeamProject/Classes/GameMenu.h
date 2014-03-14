d308a436a872bd2a59d6488e2b1713d37a011af2
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-03-14 15:53:14 +0700

    GameMenu

--- TeamProject/Classes/GameMenu.h
+++ TeamProject/Classes/GameMenu.h
@@ -10,5 +10,26 @@
 #define __TeamProject__GameMenu__
 
 #include <iostream>
+#include "cocos2d.h"
+#include "GameScreen.h"
+#include "RankingScene.h"
+using namespace cocos2d;
+class GameMenu : public CCLayer {
+private:
+    CCSize _size;
+    float _width;
+    float _height;
+    float _SIZE_RATIO;
+    float _SIZE_RATIO_X;
+    float _SIZE_RATIO_Y;
+public:
+    virtual bool init();
+    static CCScene * scene();
+    void menuCloseCallback(CCObject * pSender);
+    void menuStart(CCObject * pSender);
+    void menuRank(CCObject * pSender);
+    CREATE_FUNC(GameMenu);
+};
+
 
 #endif /* defined(__TeamProject__GameMenu__) */
